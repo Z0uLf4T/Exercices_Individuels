9320d0e4004b7a3aa73840f7912763ca37bebe70
Z0uLf4T <A.Zoulfat@gmail.com>
2024-02-09 12:36:25 +0100

    ajout de l'exo du 7 février

---
+++ Exercice individuel 7-02/html_template/js/script.js
@@ -0,0 +1,95 @@
+/* Etape 1*/
+
+function askName(){
+
+/*fenêtre pour demander le nom*/
+let userName = prompt("Quel est votre nom ?");
+console.log(userName);
+
+/*message hello concaténé à afficher*/
+let hello = "👋 Bonjour " + userName + "!";
+console.log(hello);
+
+/*affichage du message hello dans html*/
+document.body.innerHTML += `<h2>${hello}</h2>`;
+}
+
+//askName()
+
+/*Etape 2*/
+
+/*déterminer la date d'aujourd'hui*/
+const today = new Date(); 
+console.log (today);
+
+function askBirthYear(){
+
+/*fenêtre pour demander l'année de naissance*/ 
+let birthYear = prompt("Quel est votre année de naissance ?");
+console.log(birthYear);
+
+/*calcul de l'âge à partir de l'année de naissance*/
+    
+    /*déterminer l'année actuelle*/
+let currentYear = today.getFullYear();
+console.log (currentYear);
+
+    /*comparaison avec l'anné de naissance*/
+let ageCalculation = currentYear - birthYear;
+console.log(ageCalculation);
+
+/*message userAge concaténé à afficher*/
+let userAge = "Vous avez " + ageCalculation + " ans.";
+console.log(userAge);
+
+/*affichage du message userAge dans html*/
+document.body.innerHTML += `<h3>${userAge}</h3>`;
+
+/*ressortir la valeur ageCalculation pour l'utiliser dans la fonction askMonth()*/
+return ageCalculation;
+}
+
+askName()
+askBirthYear()
+/*attention le console.log de la fonction va appeler de nouveau la fonction et donc relancer une fenêtre prompt*/
+
+/*préciser le calcul de l'âge à partir du mois de naissance*/
+
+/*Ajustement de l'âge en fonction du mois de naissance*/
+
+/*fenêtre pour demander le mois de naissance*/
+let birthMonth = prompt ("Quel est votre mois de naissance ?");
+console.log(birthMonth);
+
+/*convertir en nombre*/
+
+const monthName = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Aôut","Septembre","Octobre","Novembre","Décembre"]
+
+/*boucle pour comparer birthMonth avec le tableau monthName et resortir l'index (il sera comparé à currentMonthNumber)*/
+
+let monthNumber
+
+for (let index = 0; index < monthName.length; index++) {
+    if(birthMonth == monthName[index]){
+        let monthNumber =index
+        console.log(monthNumber)
+    }  
+}
+
+console.log(monthNumber)
+
+    /*déterminer le mois actuel*/
+let currentMonthNumber = today.getMonth() + 1;
+console.log(currentMonthNumber);
+
+    /*nom du mois actuel*/
+let month = today.toLocaleString('fr-FR', { month: 'long' });
+console.log(month); 
+    
+/*comparaison du mois de naissance avec le mois actuel*/
+if (monthNumber<currentMonthNumber) {
+    console.log(askBirthYear());
+} else {
+    ;
+}
+
