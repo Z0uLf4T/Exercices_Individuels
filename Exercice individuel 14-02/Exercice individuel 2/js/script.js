750960529078de251cecdc22462969ec675592b2
Z0uLf4T <A.Zoulfat@gmail.com>
2024-02-16 10:56:53 +0100

    fonction atGuess

--- Exercice individuel 14-02/Exercice individuel 2/js/script.js
+++ Exercice individuel 14-02/Exercice individuel 2/js/script.js
@@ -27,17 +27,45 @@ if (givenNumber==22) {
     
 }
 
+//didIWin(givenNumber)
 
+
+/*Etape 3*/
 //Création de la fonction gamePlay qui rappelle les fonctions tant que le nombre n'est pas égal à 22
 
 function gamePlay(){
     askNumber();
-    console.log(givenNumber)
+    console.log(givenNumber);
     //didIWin(givenNumber);  
     while(didIWin(givenNumber)===false)
-    askNumber()
+    askNumber();
     //didIWin(givenNumber)
 }
 
 gamePlay()
+
+
+/*Etape 4*/
+
+/*fonction pour demander un nombre à deviner au player 1*/
+
+function atGuess(){
+    toBeGuessed = prompt("Entrez un nombre entre 0 et 50.");
+    console.log (toBeGuessed);
+    return toBeGuessed
+}
+
+//atGuess()
+
+
+/*fonction qui redemande un nombre tant qu'il n'est pas compris entre 0 et 50*/
+function player1(){
+    //atGuess();
+    //console.log(toBeGuessed);
+    while(atGuess()>50);
+    //atGuess();
+}
+
+player1()
+
 
